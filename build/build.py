9b883c358d37cfb9b78ab35319d601997a62397e
Austen Cameron <austen.cameron@gmail.com>
2022-04-05 20:31:06 -0600

    wip

--- build/build.py
+++ build/build.py
@@ -19,60 +19,58 @@ req = urllib.request.Request(
 docs = urllib.request.urlopen(req)
 links = re.findall('href="(/docs/.*?)"', docs.read().decode('utf-8'))
 
-print(links)
+found = []
+pages = []
 
-# found = []
-# pages = []
+# Compile all the info for the pages
+for url in sorted(set(links)):
+    topic = url.split('/')[2].title().replace('-', ' ').split('#')[0]
+    slug = url.split('/')[2].split('#')[0]
 
-# # Compile all the info for the pages
-# for url in sorted(set(links)):
-#     topic = url.split('/')[2].title().replace('-', ' ').split('#')[0]
-#     slug = url.split('/')[2].split('#')[0]
+    if (topic not in found):
+        found.append(topic)
+        pages.append({
+            "topic": topic,
+            "slug": slug,
+            "command": "tailwindDocs" + slug.replace('-', ' ').title().replace(' ', '')
+        })
 
-#     if (topic not in found):
-#         found.append(topic)
-#         pages.append({
-#             "topic": topic,
-#             "slug": slug,
-#             "command": "tailwindDocs" + slug.replace('-', ' ').title().replace(' ', '')
-#         })
+# Generate command definitions in package.json
+with open(join(abspath(dirname(__file__)), '../package.json'), 'r') as f:
+    data = json.load(f)
+    data['contributes']['commands'] = []
+    data['activationEvents'] = []
 
-# # Generate command definitions in package.json
-# with open(join(abspath(dirname(__file__)), '../package.json'), 'r') as f:
-#     data = json.load(f)
-#     data['contributes']['commands'] = []
-#     data['activationEvents'] = []
+    for page in pages:
+        data['activationEvents'].append(
+            "onCommand:extension." + page['command'])
+        data['contributes']['commands'].append({
+            "command": "extension.{}".format(page['command']),
+            "category": "Tailwind Docs",
+            "title": page['topic'],
+        })
 
-#     for page in pages:
-#         data['activationEvents'].append(
-#             "onCommand:extension." + page['command'])
-#         data['contributes']['commands'].append({
-#             "command": "extension.{}".format(page['command']),
-#             "category": "Tailwind Docs",
-#             "title": page['topic'],
-#         })
+with open(join(abspath(dirname(__file__)), '../package.json'), 'w') as f:
+    json.dump(data, f, indent=4)
 
-# with open(join(abspath(dirname(__file__)), '../package.json'), 'w') as f:
-#     json.dump(data, f, indent=4)
+# Generate the extension.ts file
+with open(join(abspath(dirname(__file__)), '../src/extension.ts'), 'w+') as f:
+    f.write("'use strict';\n")
+    f.write("import * as vscode from 'vscode';\n\n")
+    f.write("export function activate(context: vscode.ExtensionContext) {\n\n")
 
-# # Generate the extension.ts file
-# with open(join(abspath(dirname(__file__)), '../src/extension.ts'), 'w+') as f:
-#     f.write("'use strict';\n")
-#     f.write("import * as vscode from 'vscode';\n\n")
-#     f.write("export function activate(context: vscode.ExtensionContext) {\n\n")
+    for page in pages:
+        f.write(
+            '    let ' + page['command'] + ' = ' +
+            'vscode.commands.registerCommand('
+            + "'extension." + page['command'] + "', () => {\n"
+            + "        vscode.commands.executeCommand('vscode.open', "
+            + "vscode.Uri.parse('https://tailwindcss.com/docs/" + page['slug']
+            + "'));\n"
+            + "    });\n"
+            + "    context.subscriptions.push(" + page['command'] + ");\n"
+        )
 
-#     for page in pages:
-#         f.write(
-#             '    let ' + page['command'] + ' = ' +
-#             'vscode.commands.registerCommand('
-#             + "'extension." + page['command'] + "', () => {\n"
-#             + "        vscode.commands.executeCommand('vscode.open', "
-#             + "vscode.Uri.parse('https://tailwindcss.com/docs/" + page['slug']
-#             + "'));\n"
-#             + "    });\n"
-#             + "    context.subscriptions.push(" + page['command'] + ");\n"
-#         )
+    f.write("\n}")
 
-#     f.write("\n}")
-
-# print('Extension files have been built.')
+print('Extension files have been built.')
