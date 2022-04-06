ee2fc593a534ffcb7f00ade87a10ceac0aa766ea
Austen Cameron <austen.cameron@gmail.com>
2022-04-05 20:29:33 -0600

    wip

--- build/build.py
+++ build/build.py
@@ -6,7 +6,7 @@ from shutil import copyfile
 
 # Create a request with a valid User-Agent
 req = urllib.request.Request(
-    'https://tailwindcss.com/',
+    'https://tailwindcss.com/docs',
     data=None,
     headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) ' +
@@ -17,11 +17,9 @@ req = urllib.request.Request(
 
 # Pull the html from the main docs page and find all /docs links
 docs = urllib.request.urlopen(req)
-print(docs.read().decode('utf-8'))
-links = re.findall('href="(/docs/(.*?))"', docs.read().decode('utf-8'))
+links = re.findall('href="(/docs/.*?)"', docs.read().decode('utf-8'))
 
-# print(links)
-
+print(links)
 
 # found = []
 # pages = []
@@ -54,27 +52,27 @@ links = re.findall('href="(/docs/(.*?))"', docs.read().decode('utf-8'))
 #             "title": page['topic'],
 #         })
 
-# # with open(join(abspath(dirname(__file__)), '../package.json'), 'w') as f:
-# #     json.dump(data, f, indent=4)
+# with open(join(abspath(dirname(__file__)), '../package.json'), 'w') as f:
+#     json.dump(data, f, indent=4)
 
 # # Generate the extension.ts file
-# # with open(join(abspath(dirname(__file__)), '../src/extension.ts'), 'w+') as f:
-# #     f.write("'use strict';\n")
-# #     f.write("import * as vscode from 'vscode';\n\n")
-# #     f.write("export function activate(context: vscode.ExtensionContext) {\n\n")
+# with open(join(abspath(dirname(__file__)), '../src/extension.ts'), 'w+') as f:
+#     f.write("'use strict';\n")
+#     f.write("import * as vscode from 'vscode';\n\n")
+#     f.write("export function activate(context: vscode.ExtensionContext) {\n\n")
 
-# #     for page in pages:
-# #         f.write(
-# #             '    let ' + page['command'] + ' = ' +
-# #             'vscode.commands.registerCommand('
-# #             + "'extension." + page['command'] + "', () => {\n"
-# #             + "        vscode.commands.executeCommand('vscode.open', "
-# #             + "vscode.Uri.parse('https://tailwindcss.com/docs/" + page['slug']
-# #             + "'));\n"
-# #             + "    });\n"
-# #             + "    context.subscriptions.push(" + page['command'] + ");\n"
-# #         )
+#     for page in pages:
+#         f.write(
+#             '    let ' + page['command'] + ' = ' +
+#             'vscode.commands.registerCommand('
+#             + "'extension." + page['command'] + "', () => {\n"
+#             + "        vscode.commands.executeCommand('vscode.open', "
+#             + "vscode.Uri.parse('https://tailwindcss.com/docs/" + page['slug']
+#             + "'));\n"
+#             + "    });\n"
+#             + "    context.subscriptions.push(" + page['command'] + ");\n"
+#         )
 
-# #     f.write("\n}")
+#     f.write("\n}")
 
 # print('Extension files have been built.')
